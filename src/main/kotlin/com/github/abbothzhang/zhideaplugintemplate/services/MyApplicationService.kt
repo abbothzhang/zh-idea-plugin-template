bd2cd291dca2e09bc0b6b9a74d71094b4fb1e70d
GitHub Action <action@github.com>
2022-05-31 06:39:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/abbothzhang/zhideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.abbothzhang.zhideaplugintemplate.services
+
+import com.github.abbothzhang.zhideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/abbothzhang/zhideaplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/abbothzhang/zhideaplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.abbothzhang.zhideaplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.abbothzhang.zhideaplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
